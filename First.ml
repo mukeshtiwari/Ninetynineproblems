eec9572f73225eef745ddb0900fee413e6a92ce0
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2015-10-31 12:33:05 +0530

    Finish List problems

--- First.ml
+++ First.ml
@@ -77,3 +77,101 @@ let encodeList (xs : 'a list) : (int * 'a) list =
        if x = y then acc_encode acc (count + 1) (y :: t)
        else acc_encode ((count + 1, x) :: acc) 0 (y :: t)
   in List.rev (acc_encode [] 0 xs)
+
+type 'a rle =
+  |One of 'a
+  |Many of int * 'a
+
+let encodeRle (xs : 'a list) : 'a rle list =
+  List.map (fun x ->
+      let t = List.length x in
+      match t with
+      | 1 -> One (List.hd x)
+      | _ -> Many (t, List.hd x)) (pack xs)
+
+let rec decode : 'a rle list -> 'a list =
+  function
+  | [] -> []
+  | One x :: t -> x :: decode t
+  | Many (k, x) :: t ->
+     let rec iterate x = function
+       | 0 -> []
+       | w -> x :: iterate x (pred w)
+     in List.append (iterate x k) (decode t)
+
+let encodeDirectSolution (xs : 'a list) : 'a rle list =
+  let rle count x = if count = 0 then One x else Many (count, x) in
+  let rec acc_encode count acc = function
+    | [] -> []
+    | [x] -> rle count x :: acc
+    | x :: (y :: _ as t) ->
+       if x = y then acc_encode (count + 1) acc t
+       else acc_encode 0 (rle count x :: acc) t
+  in List.rev (acc_encode 0 [] xs)
+
+let rec duplicate : 'a list -> 'a list = function
+  | [] -> []
+  | x :: t -> x :: x :: duplicate t
+
+let duplicateAcc (xs : 'a list) : 'a list =
+  let rec acc_duplicate acc = function
+    | [] -> acc
+    | x :: t -> acc_duplicate (x :: x :: acc) t
+  in List.rev (acc_duplicate [] xs)
+
+let rec replicate (xs : 'a list) (k : int) : 'a list =
+  match xs with
+  | [] -> []
+  | x :: t ->
+     let rec iterate x = function
+       | 0 -> []
+       | w -> x :: iterate x (pred w)
+     in List.append (iterate x k) (replicate t k)
+
+let rec drop (xs : 'a list) (k : int) : 'a list =
+  let rec drop_k count = function
+    | [] -> []
+    | x :: t ->
+       if count = k then drop_k 1 t
+       else x :: drop_k (count + 1) t
+  in drop_k 1 xs
+
+let rec split (xs : 'a list) (k : int) : ('a list * 'a list) =
+  let rec split_two ys acc cnt =
+    if List.length ys = 0 then (List.rev acc, [])
+    else if cnt = k then (List.rev acc, ys)
+    else split_two (List.tl ys) (List.hd ys :: acc) (cnt + 1)
+  in split_two xs [] 0
+
+let slice (xs : 'a list) (n : int) (m : int) : 'a list =
+  let rec slice_nm cnt acc = function
+    | [] -> List.rev acc
+    | h :: t ->
+       if cnt < n then slice_nm (succ cnt) acc t
+       else if cnt > m then List.rev acc
+       else slice_nm (succ cnt) (h :: acc) t
+  in slice_nm 0 [] xs
+
+let rotate (xs : 'a list) (k : int) : 'a list =
+  let len = List.length xs in
+  let k' = (len + k) mod len in
+  let (u, v) = split xs k' in List.append v u
+
+let remove_at (k : int) (xs : 'a list) : 'a list =
+  let rec remove_kth cnt acc = function
+    | [] -> []
+    | h :: t ->
+       if cnt = k then List.append (List.rev acc) t
+       else remove_kth (succ cnt) (h :: acc) t
+  in remove_kth 0 [] xs
+
+let rec insert_at (elem : 'a) (k : int) (xs : 'a list) : 'a list =
+  let rec insert_kth cnt acc = function
+    | [] -> List.rev (elem :: acc)
+    | h :: t ->
+       if cnt = k then (List.rev (h :: elem :: acc)) @ t
+       else insert_kth (succ cnt) (h :: acc) t
+  in insert_kth 0 [] xs
+
+let rec range (n : int) (m : int) : int list =
+  if
